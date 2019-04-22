602ef29dd7b7fe5112d733a60a8280a97548f3d8
Gregory Sanders <gsanders87@gmail.com>
2019-04-22 14:01:01 -0400

    f'Internally define dynamic federations deployment'

--- src/versionbitsinfo.cpp
+++ src/versionbitsinfo.cpp
@@ -18,7 +18,7 @@ const struct VBDeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION_B
     {
         /*.name =*/ "segwit",
         /*.gbt_force =*/ true,
-    }
+    },
     {
         /*.name =*/ "dynafed",
         /*.gbt_force =*/ true,
